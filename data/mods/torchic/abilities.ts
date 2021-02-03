36fc2fe8ac8d55ce65c902ddad2cbcef2d4266de
Muhframos <pacocacombinacomtudo@gmail.com>
2021-02-03 15:43:13 -0300

    e

--- data/mods/torchic/abilities.ts
+++ data/mods/torchic/abilities.ts
@@ -64,13 +64,11 @@ export const Abilities: {[k: string]: ModdedAbilityData} = {
 		num: -7,
 	},
 	sidhessacrifice: {
-		onPrepareHit(source, move) {
-			source.heal(source.baseMaxhp / 3);
+		onPrepareHit(source, target, move) {
+			pokemon.heal(pokemon.baseMaxhp / 3);
+			this.add('-start', source, 'typechange', '[from] ability: Sidhes Sacrifice');
+			}
 		},
-		name: "Sidhe's Sacrifice",
-		rating: 4.5,
-		num: -8,
-	},
 	energyoverflow: {
 		onResidualOrder: 26,
 		onResidualSubOrder: 1,
