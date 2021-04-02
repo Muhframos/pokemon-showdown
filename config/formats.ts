d301bcbf22ad760701deb6fdc366401f5baea1a7
Muhframos <pacocacombinacomtudo@gmail.com>
2021-04-01 23:15:28 -0300

    format ficks

--- config/formats.ts
+++ config/formats.ts
@@ -143,7 +143,7 @@ export const Formats: FormatList = [
 		gameType: 'freeforall',
 		challengeShow: false,
 		rated: false,
-		ruleset: ['Standard NatDex', 'Species Clause', 'Dynamax Clause', 'Obtainable', 'HP Percentage Mod', 'Sleep Clause Mod'],
+		ruleset: ['Standard NatDex', 'Species Clause', 'Dynamax Clause', 'HP Percentage Mod', 'Sleep Clause Mod'],
 	},
 	{
 		name: "[Gen 8] Adventures Custom Game",
