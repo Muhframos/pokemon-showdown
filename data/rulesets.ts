b828fbeb1ece66329b9bcd6b8c366a6e1b50a68f
Muhframos <pacocacombinacomtudo@gmail.com>
2021-04-01 23:18:48 -0300

    maybe it will work now

--- data/rulesets.ts
+++ data/rulesets.ts
@@ -159,7 +159,7 @@ export const Formats: {[k: string]: FormatData} = {
 		name: 'Standard NatDex',
 		desc: "The standard ruleset for all National Dex tiers",
 		ruleset: [
-			'Obtainable', '+Unobtainable', '+Past', 'Team Preview', 'Nickname Clause', 'HP Percentage Mod', 'Cancel Mod', 'Endless Battle Clause',
+			'+Unobtainable', '+Past', 'Team Preview', 'Nickname Clause', 'Endless Battle Clause',
 		],
 		onValidateSet(set) {
 			// These Pokemon are still unobtainable
