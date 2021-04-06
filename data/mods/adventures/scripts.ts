69206e4cfd5c0b2b2b2c980238f11323ae8af67f
Muhframos <pacocacombinacomtudo@gmail.com>
2021-04-06 14:33:38 -0300

    hm

--- data/mods/adventures/scripts.ts
+++ data/mods/adventures/scripts.ts
@@ -979,22 +979,25 @@ this.modData('Learnsets', 'cradily').learnset.vinetrap = ['8L1'];
 
 //REMOVE MOVES END-
 
-//Clientside Stuff, dont worry about it.
-export const Scripts: ModdedBattleScriptsData = {
+	//Clientside Stuff, dont worry about it.
+	export const Scripts: ModdedBattleScriptsData = {
     teambuilderConfig: {
         // for micrometas to only show custom tiers
         excludeStandardTiers: true,
         // only to specify the order of custom tiers
 		adventures: { 
-		['Adventures OU', 'Adventures UU', 'Adventures Ubers', 'Adventures DOU', 'Adventures DUU' , 'Adventures 1v1', 'Adventures Free For All', 'Adventures Multi Battle', 'Adventures Custom Game', 'Adventures FFA Custom Game'],
-		ignoreEVLimits: false,
-		},
+			customTiers: ['Adventures OU', 'Adventures UU', 'Adventures Ubers', 'Adventures DOU', 'Adventures DUU' , 'Adventures 1v1', 'Adventures Free For All', 'Adventures Multi Battle', 'Adventures Custom Game', 'Adventures FFA Custom Game'],
+			ignoreEVLimits: false,
+					},
         // allow 252 EVs in every stat
-		torchic: { ['Axis League AG', 'Axis League Elite 4 (Ubers)', 'Axis League Gyms (OU)'],
-        ignoreEVLimits: false,
-				},
+		torchic: { 
+			customTiers:['Axis League AG', 'Axis League Elite 4 (Ubers)', 'Axis League Gyms (OU)'],
+			ignoreEVLimits: false,
+				 },
 			},
 		},
+		
+		
 		// Give Frustration, Return and Toxic to all gen 8 - Credit goes to Heat Enteis
 		const gen8mons = require('./../../pokedex').Pokedex;
 		const gen8monsKeys = Object.keys(gen8mons);
