d19b4dbc67d8c1b554f1e0d4d050c3db2562873c
Muhframos <pacocacombinacomtudo@gmail.com>
2021-04-06 14:43:26 -0300

    e

--- data/mods/adventures/scripts.ts
+++ data/mods/adventures/scripts.ts
@@ -1014,17 +1014,13 @@ this.modData('Learnsets', 'cradily').learnset.vinetrap = ['8L1'];
 
 	//Clientside Stuff, dont worry about it.
     teambuilderConfig: {
+        // for micrometas to only show custom tiers
+        excludeStandardTiers: true,
         // only to specify the order of custom tiers
-		adventures: { 
-			customTiers: ['Adventures OU', 'Adventures UU', 'Adventures Ubers', 'Adventures DOU', 'Adventures DUU' , 'Adventures 1v1', 'Adventures Free For All', 'Adventures Multi Battle', 'Adventures Custom Game', 'Adventures FFA Custom Game'],
-			ignoreEVLimits: false,
-			excludeStandardTiers: true,
-					},
+        customTiers: ['Adventures OU', 'Adventures UU', 'Adventures Ubers', 'Adventures 1v1', 'Adventures Free For All', 'Adventures Multi Battle', 'Adventures Custom Game', 'Adventures FFA Custom Game'],
+        customDoublesTiers: ['Adventures DOU', 'Adventures DUU',],
         // allow 252 EVs in every stat
-		torchic: { 
-			customTiers:['Axis League AG', 'Axis League Elite 4 (Ubers)', 'Axis League Gyms (OU)'],
-			ignoreEVLimits: false,
-			excludeStandardTiers: true,
-				 },
-			},
+        ignoreEVLimits: true,
+        // use sprites from a certain past gen where available
+    },
 };
