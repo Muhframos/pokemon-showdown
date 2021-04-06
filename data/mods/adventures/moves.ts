9d0fd2da041d4ed581c38faea07358c32d1740a0
Muhframos <pacocacombinacomtudo@gmail.com>
2021-04-06 16:37:28 -0300

    eeeeee

--- data/mods/adventures/moves.ts
+++ data/mods/adventures/moves.ts
@@ -1290,17 +1290,17 @@ export const Moves: {[moveid: string]: MoveData} = {
 				move.willChangeForme = true;
 			}
 		},
-		onHit(target, source, move) {
-			if (source.volatiles['choicelock'] && !source.hasAbility('sheerforce')) {
-				this.debug('removing choicelock: ' + source.volatiles['choicelock']);
-				source.removeVolatile('choicelock');
-			}
-		},
 		onAfterMoveSecondarySelf(pokemon, target, move) {
 			if (move.willChangeForme) {
 				const meloettaForme = pokemon.species.id === 'meloettapirouette' ? '' : '-Pirouette';
 				pokemon.formeChange('Meloetta' + meloettaForme, this.effect, false, '[msg]');
 				},
+		},
+		onHit(target, source, move) {
+			if (source.volatiles['choicelock'] && !source.hasAbility('sheerforce')) {
+				this.debug('removing choicelock: ' + source.volatiles['choicelock']);
+				source.removeVolatile('choicelock');
+			}
 		},
 		target: "allAdjacentFoes",
 		type: "Normal",
