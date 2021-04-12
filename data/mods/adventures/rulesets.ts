ab72b146feaad4e440d32697989f8e5f36da31e6
Muhframos <pacocacombinacomtudo@gmail.com>
2021-04-12 20:15:36 -0300

    Revert "ok"
    This reverts commit bbff259a374aa1f54b69fd5db9038eb395fa52d4.

---
+++ data/mods/adventures/rulesets.ts
@@ -0,0 +1,38 @@
+// Note: These are the rules that formats use
+// The list of formats is stored in config/formats.js
+export const Formats: {[k: string]: FormatData} = {
+
+	// Rulesets
+	///////////////////////////////////////////////////////////////////
+
+	standardnatdex: {
+		inherit: true,
+		onValidateSet(set) {
+			// These Pokemon are still unobtainable
+			const unobtainables = [
+				'Pichu-Spiky-eared', 'Pikachu-Belle', 'Pikachu-Cosplay', 'Pikachu-Libre',
+				'Pikachu-PhD', 'Pikachu-Pop-Star', 'Pikachu-Rock-Star', 'Eternatus-Eternamax'
+			];
+			const species = this.dex.getSpecies(set.species);
+			if (unobtainables.includes(species.name)) {
+				if (this.ruleTable.has(`+pokemon:${species.id}`)) return;
+				return [`${set.name || set.species} does not exist in the National Dex.`];
+			}
+			if (species.tier === "Unreleased") {
+				const basePokemon = this.toID(species.baseSpecies);
+				if (this.ruleTable.has(`+pokemon:${species.id}`) || this.ruleTable.has(`+basepokemon:${basePokemon}`)) {
+					return;
+				}
+				return [`${set.name || set.species} does not exist in the National Dex.`];
+			}
+			// Items other than Z-Crystals and Pokémon-specific items should be illegal
+			if (!set.item) return;
+			const item = this.dex.getItem(set.item);
+			if (!item.isNonstandard) return;
+			if (['Past', 'Unobtainable'].includes(item.isNonstandard) && !item.zMove && !item.itemUser && !item.forcedForme) {
+				if (this.ruleTable.has(`+item:${item.id}`)) return;
+				return [`${set.name}'s item ${item.name} does not exist in Gen ${this.dex.gen}.`];
+			}
+		},
+	},
+};
